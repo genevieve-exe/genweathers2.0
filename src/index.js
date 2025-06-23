58650fee02696da628675242d5f1e59faa3fb50a
genevieve-exe <genevievesandra2005@gmail.com>
2025-06-23 12:18:42 +0200

    Added axios.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+function searchCity(city) {
+    let apiKey = `73a2286e35ab2408d94of591bf48e09t`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+}
