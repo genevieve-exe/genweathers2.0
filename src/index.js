c3986332e4df3ef88eb79778a62f7ff643ab0bec
genevieve-exe <genevievesandra2005@gmail.com>
2025-06-23 13:28:04 +0200

    Added basic JS functions.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 function searchCity(city) {
-    let apiKey = `73a2286e35ab2408d94of591bf48e09t`;
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    let apiKey = "73a2286e35ab2408d94of591bf48e09t";
+    let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial";
+    console.log(apiUrl);
 }
+
+function searchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#weather-app-city");
+    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchSubmit);
